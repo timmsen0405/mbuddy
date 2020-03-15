3e1ab7a61da5f640382646b28613d016587fe520
Tim Rohnacher <cas296254@cas.dhbw.de>
2020-03-15 10:39:57 +0100

    test

--- js/index.js
+++ js/index.js
@@ -88,5 +88,5 @@ $$('.fill-form-from-data').on('click', function () {
         'cp-email': 'max@mustermann.de'
     }
     console.log(formData);
-    app.form.fillFromData('#form-kna', formData);
+    mbuddy.form.fillFromData('#form-kna', formData);
 });
